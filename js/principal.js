652f9b4c908c5e5805880ce0e53b2dcb67d0ad6a
Sarah Klock Mauricio <51770038+sarahklock@users.noreply.github.com>
2021-10-17 15:52:50 -0300

    Organizando o código com comentarios

--- js/principal.js
+++ js/principal.js
@@ -1,45 +1,50 @@
-    var botaoAdicionar = document.querySelector("#adicionar-paciente")
-        botaoAdicionar.addEventListener("click", function(){
-            event.preventDefault()    
-            var form = document.querySelector("#form-adiciona")      
-
-            var paciente = obtemPacienteDoFormulario(form)
-            var pacienteTr = montaTr(paciente)
-            
-            var tabela = document.querySelector("#tabela-pacientes");
-
-            tabela.appendChild(pacienteTr);
-        })
-        
-    function obtemPacienteDoFormulario(form) {
-        var paciente = {
-             nome : form.nome.value,
-             peso : form.peso.value,
-             altura : form.altura.value,
-             gordura : form.gordura.value,
-             imc: calculaImc(form.peso.value, form.altura.value)
-        }
-        return paciente
+/* Coleta as informações sobre um paciente atravez do formulario. */
+function obtemPacienteDoFormulario(form) {
+    var paciente = {
+         nome : form.nome.value,
+         peso : form.peso.value,
+         altura : form.altura.value,
+         gordura : form.gordura.value,
+         imc: calculaImc(form.peso.value, form.altura.value)
     }
+    return paciente
+}
 
-    function montaTr(paciente){
-        var pacienteTr = document.createElement("tr");
+/* Cria uma linha na tabela com as informações do paciente. */
+function montaTr(paciente){
+    var pacienteTr = document.createElement("tr");
 
-        pacienteTr.classList.add("paciente")
+    pacienteTr.classList.add("paciente")
 
-        pacienteTr.appendChild(montaTd(paciente.nome, "info-nome"))
-        pacienteTr.appendChild(montaTd(paciente.peso, "info-peso"))
-        pacienteTr.appendChild(montaTd(paciente.altura, "info-altura"))
-        pacienteTr.appendChild(montaTd(paciente.gordura, "info-gordura"))
-        pacienteTr.appendChild(montaTd(paciente.imc, "info-imc"))
+    pacienteTr.appendChild(montaTd(paciente.nome, "info-nome"))
+    pacienteTr.appendChild(montaTd(paciente.peso, "info-peso"))
+    pacienteTr.appendChild(montaTd(paciente.altura, "info-altura"))
+    pacienteTr.appendChild(montaTd(paciente.gordura, "info-gordura"))
+    pacienteTr.appendChild(montaTd(paciente.imc, "info-imc"))
 
-        return pacienteTr
-    }
+    return pacienteTr
+}
 
-    function montaTd(dado, classe) {
-        var td = document.createElement("td")
-        td.classList.add(classe)
-        td.textContent = dado
+/* Cria uma cédula com um dado em uma linha de um paciente. */
+function montaTd(dado, classe) {
+    var td = document.createElement("td")
+    td.classList.add(classe)
+    td.textContent = dado
 
-        return td
-    }
+    return td
+}
+
+/* Ao clicar no botão adicionar, cria-se uma linha na tabela com os dados inseridos. */
+var botaoAdicionar = document.querySelector("#adicionar-paciente")
+
+botaoAdicionar.addEventListener("click", function(){
+    event.preventDefault()    
+    var form = document.querySelector("#form-adiciona")      
+
+    var paciente = obtemPacienteDoFormulario(form)
+    var pacienteTr = montaTr(paciente)
+
+    var tabela = document.querySelector("#tabela-pacientes");
+
+    tabela.appendChild(pacienteTr);
+})
